921d45f3f40660b3320cd6487935c552dc0c5eb6
shuradavi/macos <shuradavi@gmail.com>
2024-09-25 10:26:27 +0300

    added fonts, typification ConverterSide props

--- src/App.tsx
+++ src/App.tsx
@@ -7,49 +7,51 @@ import ConverterSide from './components/ConverterSide';
 
 function App() {
     const [currencies, setCurrencies] = useState<null | Valute[]>(null);  // Список наименований валют
-    
     const [fromCurrencyName, setFromCurrencyName] = useState<string>('RUR');    // Имя меняемой валюты
     const [fromValue, setFromValue] = useState<number>(1);  // Количество меняемой валюты
-
 	const [toCurrencyName, setToCurrencyName] = useState<string>('USD');    // Имя получаемой валюты
 	const [toValue, setToValue] = useState<undefined | number>();   // Количество получаемой валюты
     useEffect(() => {
         if (!Boolean(currencies)) {
-            getData(pathToAPI)
+            getData(pathToAPI)        // решить проблему с memory leak
                 .then(res => {
-                    const arrayOfCurrencies: Valute[] = getAnArrayOfCurrencies(res)
-                    const sortedArrayOfCurrencies: Valute[] = sortTheArrayOfCurrensies(arrayOfCurrencies)    
+                    const arrayOfCurrencies = getAnArrayOfCurrencies(res)
+                    const sortedArrayOfCurrencies = sortTheArrayOfCurrensies(arrayOfCurrencies)    
                     setCurrencies(sortedArrayOfCurrencies);
                 })
         }
         // onChangeFromValue(fromValue)
     })  
 
-    // const onChangeFromValue = (value) => {
-	// 	if (Boolean(currencies) && Boolean(currencies.length)) {
-	// 		const fromCurrencyRate = currencies.find(cur => cur.CharCode === fromCurrencyName);
-	// 		const toCurrencyRate = currencies.find(cur => cur.CharCode === toCurrencyName);
-	// 		const rateFrom = fromCurrencyRate.Value / fromCurrencyRate.Nominal; //		Курс входящей валюты к рублю
-	// 		const rateTo = toCurrencyRate.Value / toCurrencyRate.Nominal;
-	// 		const result = value * (rateFrom / rateTo)
-	// 			Number.isInteger(result)
-	// 			?	setToValue(result)
-	// 			:	setToValue(result.toFixed(4))
-	// 		setFromValue(value); 
-	// 	}
-	// }
+    const onChangeFromValue = (value: number) => {
+        if (currencies !== null && Boolean(currencies.length)) {
+            const fromCurrencyData = currencies.find(cur => cur.CharCode === fromCurrencyName)!;
+            const toCurrencyData = currencies.find(cur => cur.CharCode === toCurrencyName)!
+            const rateFrom = fromCurrencyData.Value / fromCurrencyData.Nominal
+        }
+    }
+   
     return (
         <>
             <h1>Конвертер валют</h1>
             <div className='converter_main_wrapper'>
                 <div className='converter_side_wrapper'> 
-                    <ConverterSide />
+                    <ConverterSide title={'У меня есть'}
+                        inputValue={fromValue}
+                        currencies={currencies}
+                        activeCurrencyName={fromCurrencyName}
+                        exchageCurrencyName={toCurrencyName}
+                    />
 			  	</div>
                 <div className="converter_center_wrapper">
                     <div className="converter_center_reverse"></div>
                 </div>
                 <div className='converter_side_wrapper'>
-                    <ConverterSide />
+                    <ConverterSide title={'Хочу приобрести'}
+                     inputValue={toValue}
+                     currencies={currencies}
+                     activeCurrencyName={toCurrencyName}
+                     exchageCurrencyName={fromCurrencyName}/>
                 </div>
             </div>
     </>
--- src/components/ConverterSide.tsx
+++ src/components/ConverterSide.tsx
@@ -1,16 +1,42 @@
 import React, { useState } from "react";
 import { defaultPanelCurrencies } from "../defaultValues/defaultValues";
+import { Valute } from "../types/types";
 
-const ConverterSide = () => {
+interface ConverterSideProps {
+    title: string;
+    inputValue: undefined | number;
+    currencies: null | Valute[];
+    activeCurrencyName: string;
+    exchageCurrencyName: string;
+}
+
+const ConverterSide: React.FC<ConverterSideProps> = (props) => {
+    const { title, inputValue, currencies, activeCurrencyName, exchageCurrencyName } = props
+    
     const [onPanelCurrencies, setOnPanelCurrencies] = useState<string[]>(defaultPanelCurrencies); // Наименования валют на панели
     const [isFormOpen, setFormOpen] = useState<boolean>(false); // Сосояние формы с таблицей валют
 
+    const onSelectClickHandler = () => {
+        if (!isFormOpen) {
+            setFormOpen(true)
+        }
+    }
+    
+    const closeForm = () => {
+        if (isFormOpen) {
+            setFormOpen(false)
+        }
+    }
+
+    const onPanelCurrencyClickHandler = (cur: string) => {
+    }
+
     return (
         <div className="converter_side">
-            <div className="side_title">
-                Title
+            <div className="side_title  title_font">
+                {title}
             </div>
-            <div className="side_switcher">
+            <div className="side_switcher defaut_font">
 
             </div>
             <div className="side_input_box">
